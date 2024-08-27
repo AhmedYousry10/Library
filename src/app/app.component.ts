81161b741401e50c2eb98f5058d6a200bc2f6632
Ahmed Yousry Helal <ahmedu3helal@gmail.com>
2024-08-27 15:11:10 +0300

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , NavbarComponent, CommonModule , RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
